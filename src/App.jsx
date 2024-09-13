49e5ba0d5a68beb0a3e2774c588f2e9b51fb8d35
Alec Mirambeau <alecmirambeau@gmail.com>
2024-09-13 17:25:14 -0400

    test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Vite and React!</h1>
+      <p>This is a simple demo React app using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
